7d84bafa14bcfe149b5b5c75779eb385e6f3e299
Saad Lahrichi <slxg3@umsystem.edu>
2025-09-22 13:11:49 -0500

    REMOVE: complexPytorch imports

--- core/models/CVNN.py
+++ core/models/CVNN.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-from complexPyTorch.complexFunctions import complex_relu
+#from complexPyTorch.complexFunctions import complex_relu
 
 class ComplexLinearFinal(nn.Module):
     """Complex-valued Linear Layer."""
